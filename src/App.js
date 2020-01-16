0dcd114bc8864d53530115342c9125fe180e53c0
w3bdesign <hijack_xxx@yahoo.com>
2020-01-16 02:45:57 +0100

    Finished basic layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Table } from 'antd';
+import { Table, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 const data = [
@@ -35,7 +35,12 @@ const columns = [
     key: 'action',
     render: (text, record) => (
       <span>
-        <a href="#delete">
+        <a
+          href="#delete"
+          onClick={() => {
+            alert('You clicked on delete!');
+          }}
+        >
           <span role="img" aria-label="delete">
             ❌
           </span>
@@ -46,9 +51,28 @@ const columns = [
 ];
 
 function App() {
+  // <Table dataSource={data} columns={columns} />;
+
+  const [todos, setTodos] = useState([
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',      
+    }
+  ]);
+  //const testDaniel = todos.map(test => {test});
+  //console.log(testDaniel);
+  // https://next.ant.design/components/table/#components-table-demo-row-selection
   return (
     <div className="App">
-      <Table dataSource={data} columns={columns} />;
+      <Table dataSource={data} columns={columns} />
+      <br />
+      <div className="todo">
+      <h3>Add TODO</h3>
+      <Input placeholder="Add TODO" />
+      <Button type="primary" block>Add</Button>
+      </div>
+     
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Table } from 'antd';
 
-test('renders learn react link', () => {
+test('Renders Add TODO', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Add TODO/i);
+  //expect(linkElement).toBeVisible();
   expect(linkElement).toBeInTheDocument();
 });
