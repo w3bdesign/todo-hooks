1d4878dfd827872d8102b5f00dea17ea2a64a492
w3bdesign <hijack_xxx@yahoo.com>
2020-01-16 07:16:26 +0100

    Update form.component.jsx

--- src/components/Form/form.component.jsx
+++ src/components/Form/form.component.jsx
@@ -1,24 +1,35 @@
-import React from "react";
-import {  Input, Button, Form } from 'antd';
+import React from 'react';
+import { Input, Button, Form } from 'antd';
 
-function TodoForm () {
-    //const [form] = Form.useForm();
-return (
-    <div>
-        <Form onFinish={() => { alert("Form submitted!") }}>
-    <h3>Add TODO</h3>
-    <Input placeholder="Enter text here..." />
-    <Button
-      type="primary"      
-      block
+function TodoForm() {
+  //const [form] = Form.useForm();
+  return (
+    <Form
+    onSubmit={() => {
+        alert('Form submitted in onSubmit!');
+      }}
+      // TODO onFinish is not working?
+      onFinish={() => {
+        alert('Form submitted!');
+      }}
     >
-      Add
-    </Button>
+      <h3>Add TODO</h3>      
+      <Form.Item       
+        name="todotext"
+        rules={[
+          {
+            required: true,
+            message: 'Please input text!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Button type="primary" block>
+        Add
+      </Button>
     </Form>
-    </div>
-);
-    }
-
-
+  );
+}
 
 export default TodoForm;
