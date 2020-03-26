beb861401bf66b9ac18690498311c7731de285f4
w3bdesign <hijack_xxx@yahoo.com>
2020-03-27 00:35:04 +0100

    Remove unnecessary code from Title component

--- src/components/Title/Title.component.jsx
+++ src/components/Title/Title.component.jsx
@@ -1,31 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Typography } from 'antd';
-
-import { TodoContext } from '../../App';
 
 const { Paragraph } = Typography;
 
 export const Title = ({ children, record }) => {
-  //const [todos, dispatchTodos] = useContext(TodoContext);
-  //const [todos] = useContext(TodoContext);
-
-
-
-  //const foundCompleted = todos.find(record.key) ?? 'false';
-  //console.log(todos.find(record.key));
-
-  //const findMe = element => element.key === todos.key;
-  //console.log("Record completed: ");
-  //console.log(todos.completed);
-  //console.log(record.completed);
-
-  //const foundCompleted = todos.find(findMe) ?? 'false';
-  //console.log(foundCompleted);
-
-  //  foundCompleted.completed === 'true' ? 'true' : 'false'
-
-  return <Paragraph  className={
-    // Display text-decoration: line-through if TODO is completed (true)
-    record.completed === 'true' ? 'true' : 'false'
-  }>{children}</Paragraph>;
+  return (
+    <Paragraph
+      className={
+        // Display text-decoration: line-through if TODO is completed (true)
+        record.completed === 'true' ? 'true' : 'false'
+      }
+    >
+      {children}
+    </Paragraph>
+  );
 };
