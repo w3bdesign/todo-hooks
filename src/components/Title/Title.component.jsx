526d660edcf6117477dc115cd7730288d7508e09
w3bdesign <hijack_xxx@yahoo.com>
2020-03-26 23:42:32 +0100

    We can now complete TODOs

--- src/components/Title/Title.component.jsx
+++ src/components/Title/Title.component.jsx
@@ -6,19 +6,26 @@ import { TodoContext } from '../../App';
 const { Paragraph } = Typography;
 
 export const Title = ({ children, record }) => {
-  const [todos, dispatchTodos] = useContext(TodoContext);
+  //const [todos, dispatchTodos] = useContext(TodoContext);
+  //const [todos] = useContext(TodoContext);
 
-  console.log('Title');
-  console.log(record.key);
+
 
   //const foundCompleted = todos.find(record.key) ?? 'false';
   //console.log(todos.find(record.key));
 
-  const findMe = element => element.key === todos.key;
-  const foundCompleted = todos.find(findMe) ?? 'false';
+  //const findMe = element => element.key === todos.key;
+  //console.log("Record completed: ");
+  //console.log(todos.completed);
+  //console.log(record.completed);
+
+  //const foundCompleted = todos.find(findMe) ?? 'false';
+  //console.log(foundCompleted);
+
+  //  foundCompleted.completed === 'true' ? 'true' : 'false'
 
   return <Paragraph  className={
     // Display text-decoration: line-through if TODO is completed (true)
-    foundCompleted.completed === 'true' ? 'true' : 'false'
+    record.completed === 'true' ? 'true' : 'false'
   }>{children}</Paragraph>;
 };
