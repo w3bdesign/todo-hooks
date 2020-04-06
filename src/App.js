b909c1d946673b975f1e1b8f945b4a5118a6c460
w3bdesign <hijack_xxx@yahoo.com>
2020-04-06 03:56:36 +0200

    Reset code to before Firebase implementation
    I am looking into using hooks instead of class based components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import 'antd/dist/antd.css';
 import './App.css';
 
 import { TodoForm } from './components/Form/Form.component';
-import { Google } from './components/Form/Google.component';
-
 import { todoReducer } from './utils/functions/formReducer';
-//import { firebaseContext } from './utils/firebase/firebaseContext';
-import Firebase, { FirebaseContext } from './utils/firebase/';
 
 import { FORM_INITIAL_STATE } from './utils/constants/INITIAL_STATE';
 import { FORM_COLUMNS } from './utils/constants/FORM_COLUMNS';
@@ -20,11 +16,6 @@ const App = () => {
   const [todos, dispatchTodos] = useReducer(todoReducer, FORM_INITIAL_STATE);
   return (
     <TodoContext.Provider value={[todos, dispatchTodos]}>
-      <FirebaseContext.Provider value={new Firebase()}>
-      <Google />
-      <Row>
-        <Col>&nbsp;</Col>
-      </Row>
       <Row type="flex" justify="center">
         <Col xs={24} sm={24} md={24} lg={12} xl={12}>
           <Table dataSource={todos} columns={FORM_COLUMNS} />
@@ -35,7 +26,6 @@ const App = () => {
           <TodoForm />
         </Col>
       </Row>
-      </FirebaseContext.Provider>
     </TodoContext.Provider>
   );
 };
