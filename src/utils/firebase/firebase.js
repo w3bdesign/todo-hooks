e3b26a45867c11f1e076673db175e6e98bdda83e
w3bdesign <hijack_xxx@yahoo.com>
2020-04-06 04:42:26 +0200

    Update firebase.js

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -6,7 +6,7 @@ Read the following links:
 - https://github.com/CSFrequency/react-firebase-hooks
 - https://www.npmjs.com/package/react-firebase-hooks
 - https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
-- https://github.com/jln-dk/react-use-firebase-hook
+- https://github.com/jln-dk/react-use-firebase-hook <- Install?
 
 */
 
