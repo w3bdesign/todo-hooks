7b3d7c9a3f5fb6c722454f85cac00ebe2ecb334b
w3bdesign <hijack_xxx@yahoo.com>
2020-04-06 04:05:46 +0200

    Add Firebase hook links

---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,10 @@
+/*
+
+Read the following links:
+
+- https://blog.logrocket.com/react-hooks-with-firebase-firestore
+- https://github.com/CSFrequency/react-firebase-hooks
+- https://www.npmjs.com/package/react-firebase-hooks
+- https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
+
+*/
