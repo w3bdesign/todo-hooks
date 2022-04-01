ea540e33d3f904fa88e53b53b147e39465f7ae76
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-04-01 04:06:52 +0200

    Revert "React 18"
    This reverts commit ef839a064cf0117982e3bf0580aab2377bb79ce8.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
