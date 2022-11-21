e66da6835cb7047ef3da322dbb5e7cb0cb19af5f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-21 05:42:27 +0100

    Antd 5

--- src/utils/constants/INITIAL_STATE.js
+++ src/utils/constants/INITIAL_STATE.js
@@ -4,7 +4,7 @@ export const FORM_INITIAL_STATE = [
   {
     key: uuidv4(),
     title:
-      "Use Hooks in a React application (Default TODO from INITIAL_STATE.js)",
+      "Default TODO from INITIAL_STATE.js",
     date: new Date().toISOString().slice(0, 10),
     completed: "false",
     dataIndex: 0,
