762ce112b1f2a8719d84e6bca3c41a58636eb80a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-26 04:47:07 +0100

    Npm refresh & Prettier

--- src/utils/constants/INITIAL_STATE.js
+++ src/utils/constants/INITIAL_STATE.js
@@ -3,8 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export const FORM_INITIAL_STATE = [
   {
     key: uuidv4(),
-    title:
-      "Default TODO from INITIAL_STATE.js",
+    title: "Default TODO from INITIAL_STATE.js",
     date: new Date().toISOString().slice(0, 10),
     completed: "false",
     dataIndex: 0,
